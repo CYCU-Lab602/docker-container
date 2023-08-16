b79984e58de2ca168937f36d40f4f51c2c0b234c
CY <c0975063375@gmail.com>
2023-08-16 15:31:25 +0800

    Update version.py

--- vnc/images/version.py
+++ vnc/images/version.py
@@ -7,10 +7,5 @@ runtime = 沒有包含cuda的nvcc
 devel = 有包含cuda的nvcc
 '''
 
-support_images = [  'nvidia/cuda:11.4.1-cudnn8-devel-ubuntu18.04',
-                    'nvidia/cuda:11.4.1-cudnn8-devel-ubuntu20.04',
-                    'nvidia/cuda:11.4.0-cudnn8-devel-ubuntu18.04',
-                    'nvidia/cuda:11.4.0-cudnn8-devel-ubuntu20.04',
-                    'nvidia/cuda:11.0-cudnn8-devel-ubuntu18.04',
-                    'nvidia/cuda:11.4.3-cudnn8-devel-ubuntu20.04',
+support_images = [  'nvidia/cuda:11.4.3-cudnn8-devel-ubuntu20.04',
                 ]
