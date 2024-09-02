1006d861b938dd9321c84373f43b52483d71489c
Noah <noah@c-link.com.tw>
2024-09-02 11:54:22 +0800

    update: format files.

--- vnc/data/modified/startup.sh
+++ vnc/data/modified/startup.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
 if [ -n "$VNC_PASSWORD" ]; then
-    echo -n "$VNC_PASSWORD" > /.password1
-    x11vnc -storepasswd "$(cat /.password1)" /.password2
-    chmod 400 /.password*
-    sed -i 's/^command=x11vnc.*/& -rfbauth \/.password2/' /etc/supervisor/conf.d/supervisord.conf
-    export VNC_PASSWORD=
+	echo -n "$VNC_PASSWORD" >/.password1
+	x11vnc -storepasswd "$(cat /.password1)" /.password2
+	chmod 400 /.password*
+	sed -i 's/^command=x11vnc.*/& -rfbauth \/.password2/' /etc/supervisor/conf.d/supervisord.conf
+	export VNC_PASSWORD=
 fi
 
 if [ -n "$X11VNC_ARGS" ]; then
-    sed -i "s/^command=x11vnc.*/& ${X11VNC_ARGS}/" /etc/supervisor/conf.d/supervisord.conf
+	sed -i "s/^command=x11vnc.*/& ${X11VNC_ARGS}/" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 if [ -n "$OPENBOX_ARGS" ]; then
-    sed -i "s#^command=/usr/bin/openbox\$#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
+	sed -i "s#^command=/usr/bin/openbox\$#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 if [ -n "$RESOLUTION" ]; then
-    sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
+	sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
 fi
 
 USER=${USER:-root}
 HOME=/root
 if [ "$USER" != "root" ]; then
-    echo "* enable custom user: $USER"
-    useradd --create-home --shell /usr/bin/fish --user-group --groups adm,sudo "$USER"
-    if [ -z "$PASSWORD" ]; then
-        echo "  set default password to \"ubuntu\""
-        PASSWORD=ubuntu
-    fi
-    HOME=/home/$USER
-    echo "$USER:$PASSWORD" | chpasswd
-    cp -r /root/{.config,.gtkrc-2.0,.asoundrc} "${HOME}"
-    chown -R "$USER":"$USER" "${HOME}"
-    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+	echo "* enable custom user: $USER"
+	useradd --create-home --shell /usr/bin/fish --user-group --groups adm,sudo "$USER"
+	if [ -z "$PASSWORD" ]; then
+		echo "  set default password to \"ubuntu\""
+		PASSWORD=ubuntu
+	fi
+	HOME=/home/$USER
+	echo "$USER:$PASSWORD" | chpasswd
+	cp -r /root/{.config,.gtkrc-2.0,.asoundrc} "${HOME}"
+	chown -R "$USER":"$USER" "${HOME}"
+	[ -d "/dev/snd" ] && chgrp -R adm /dev/snd
 fi
 sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
 
 # home folder
 if [ ! -x "$HOME/.config/pcmanfm/LXDE/" ]; then
-    mkdir -p "$HOME/.config/pcmanfm/LXDE/"
-    ln -sf /usr/local/share/doro-lxde-wallpapers/desktop-items-0.conf "$HOME"/.config/pcmanfm/LXDE/
-    chown -R "$USER":"$USER" "$HOME"
+	mkdir -p "$HOME/.config/pcmanfm/LXDE/"
+	ln -sf /usr/local/share/doro-lxde-wallpapers/desktop-items-0.conf "$HOME"/.config/pcmanfm/LXDE/
+	chown -R "$USER":"$USER" "$HOME"
 fi
 
 # nginx workers
@@ -49,21 +49,21 @@ sed -i 's|worker_processes .*|worker_processes 1;|' /etc/nginx/nginx.conf
 
 # nginx ssl
 if [ -n "$SSL_PORT" ] && [ -e "/etc/nginx/ssl/nginx.key" ]; then
-    echo "* enable SSL"
+	echo "* enable SSL"
 	sed -i 's|#_SSL_PORT_#\(.*\)443\(.*\)|\1'"$SSL_PORT"'\2|' /etc/nginx/sites-enabled/default
 	sed -i 's|#_SSL_PORT_#||' /etc/nginx/sites-enabled/default
 fi
 
 # nginx http base authentication
 if [ -n "$HTTP_PASSWORD" ]; then
-    echo "* enable HTTP base authentication"
-    htpasswd -bc /etc/nginx/.htpasswd "$USER" "$HTTP_PASSWORD"
+	echo "* enable HTTP base authentication"
+	htpasswd -bc /etc/nginx/.htpasswd "$USER" "$HTTP_PASSWORD"
 	sed -i 's|#_HTTP_PASSWORD_#||' /etc/nginx/sites-enabled/default
 fi
 
 # dynamic prefix path renaming
 if [ -n "$RELATIVE_URL_ROOT" ]; then
-    echo "* enable RELATIVE_URL_ROOT: $RELATIVE_URL_ROOT"
+	echo "* enable RELATIVE_URL_ROOT: $RELATIVE_URL_ROOT"
 	sed -i 's|#_RELATIVE_URL_ROOT_||' /etc/nginx/sites-enabled/default
 	sed -i 's|_RELATIVE_URL_ROOT_|'"$RELATIVE_URL_ROOT"'|' /etc/nginx/sites-enabled/default
 fi
@@ -77,9 +77,8 @@ HTTP_PASSWORD=
 
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
-    touch /$CONTAINER_FIRST_STARTUP
+	touch /$CONTAINER_FIRST_STARTUP
 	/opt/conda/bin/conda init --all --system
 fi
 
 exec /bin/tini -s -- supervisord -n -c /etc/supervisor/supervisord.conf
-
